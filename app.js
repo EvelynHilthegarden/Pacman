425db4947b70732de87f182fdd5033d95c7a8f5f
luan-feitosa <146037049+luan-feitosa@users.noreply.github.com>
2023-10-10 21:36:34 -0300

    Update app.js
    Função createBoard agora é imutável

--- app.js
+++ app.js
@@ -68,23 +68,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const squares = [];
 
   //create your board
-  function createBoard() {
-    for (let i = 0; i < layout.length; i++) {
-      const square = document.createElement("div");
-      grid.appendChild(square);
-      squares.push(square);
-
-      //add layout to the board
-      if (layout[i] === 0) {
-        squares[i].classList.add("pac-dot");
-      } else if (layout[i] === 1) {
-        squares[i].classList.add("wall");
-      } else if (layout[i] === 2) {
-        squares[i].classList.add("ghost-lair");
-      } else if (layout[i] === 3) {
-        squares[i].classList.add("power-pellet");
-      }
+  const createSquares = (x) => {
+    const square = document.createElement("div");
+    grid.appendChild(square);
+    
+    if(x === 0) {
+      square.classList.add('pac-dot')
+    } else if (x === 1) {
+      square.classList.add('wall')
+    } else if (x === 2) {
+      square.classList.add('ghost-lair')
+    } else if (x === 3) {
+      square.classList.add('power-pellet')
     }
+    squares.push(square);
   }
   createBoard();
 
